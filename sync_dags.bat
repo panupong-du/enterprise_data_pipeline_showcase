9f687fbf4f46248ae2c09886fc479d72cc9b8334
panupong-dur <panupong-dur@users.noreply.github.com>
2026-07-09 20:23:59 +0700

    Update sync_dags.bat with new data_engineering_hub container names

--- sync_dags.bat
+++ sync_dags.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Syncing DAG files to Airflow containers...
-docker cp dags/. enterprise_data_pipeline_showcase-airflow-scheduler-1:/opt/airflow/dags/
-docker cp dags/. enterprise_data_pipeline_showcase-airflow-webserver-1:/opt/airflow/dags/
-docker cp dags/. enterprise_data_pipeline_showcase-airflow-worker-1:/opt/airflow/dags/
+docker cp dags/. data_engineering_hub-airflow-scheduler-1:/opt/airflow/dags/
+docker cp dags/. data_engineering_hub-airflow-webserver-1:/opt/airflow/dags/
+docker cp dags/. data_engineering_hub-airflow-worker-1:/opt/airflow/dags/
 echo Sync complete! 
 echo Please wait a few seconds and refresh the Airflow UI.
 pause
